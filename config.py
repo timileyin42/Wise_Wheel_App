951d0a6866f1672f561327e70359493fef739db4
timileyin42 <akanmuibro@gmail.com>
2024-05-31 18:21:55 +0000

    fixing

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-need_the_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
     TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
@@ -10,4 +14,3 @@ class Config:
     VERIFY_SERVICE_ID = os.environ.get('VERIFY_SERVICE_ID')
     PAYSTACK_PUBLIC_KEY = os.environ.get('PAYSTACK_PUBLIC_KEY')
     PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
-
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,7 +1,20 @@
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, RentalForm, PaymentForm
 
 def test_registration_form():
     form = RegistrationForm(data={
         'username': 'Alex',
         'email': 'alexbuddy@gmail.com',
-        'phone': '08124799371
+        'phone': '08124799371',
+        'password': 'securepassword',
+        'confirm_password': 'securepassword'
+    })
+
+    form.validate()
+
+    assert form.username.data == 'Alex'
+    assert form.email.data == 'alexbuddy@gmail.com'
+    assert form.phone.data == '08124799371'
+    assert form.password.data == 'securepassword'
+    assert form.confirm_password.data == 'securepassword
+
+    assert form.validate_on_submit()
